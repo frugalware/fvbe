d3db71654408a70179760ecfee54bc11ce00dc8d
James Buren <ryuo@frugalware.org>
2013-03-25 00:49:20 -0500

    raid: commit initial code which initializes module globals

--- src/raid.c
+++ src/raid.c
@@ -17,8 +17,94 @@
 
 #include "local.h"
 
+static struct device **unused = 0;
+static struct raid **used = 0;
+
+static bool raid_setup(void)
+{
+  struct device **devices = 0;
+  int size = 4096;
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  char path[PATH_MAX] = {0};
+
+  if((devices = device_probe_all(true,true)) == 0)
+    return false;  
+
+  unused = alloc(struct device *,size);
+  
+  used = alloc(struct raid *,size);
+
+  for( ; devices[i] != 0 ; ++i )
+  {
+    struct device *device = devices[i];
+    struct disk *disk = disk_open(device);
+    struct raid *raid = raid_open(device);
+    
+    if(disk != 0)
+    {
+      for( int l = 0, m = disk_partition_get_count(disk) ; l < m ; ++l )
+      {
+        if(j == size - 1)
+          continue;
+      
+        if(strcmp(disk_partition_get_purpose(disk,l),"raid") == 0)
+        {
+          strfcpy(path,sizeof(path),"%s%d",device_get_path(device),disk_partition_get_number(disk,l));
+          
+          if((unused[j] = device_open(path)) != 0)
+            ++j;
+        }
+      }
+      
+      disk_close(disk,false);
+      
+      device_close(device);
+      
+      continue;
+    }
+    else if(raid != 0)
+    {
+      if(k == size - 1)
+      {
+        raid_close(raid,false);
+      
+        device_close(device);
+      
+        continue;
+      }
+    
+      used[k++] = raid;
+      
+      continue;
+    }
+    else
+    {
+      device_close(device);
+      
+      continue;
+    }
+  }
+
+  unused[j] = 0;
+  
+  unused = redim(unused,struct device *,j + 1);
+  
+  used[k] = 0;
+
+  used = redim(used,struct raid *,k + 1);
+
+  free(devices);
+
+  return true;
+}
+
 static bool raid_run(void)
 {
+  if(!raid_setup())
+    return false;
+
   return true;
 }
 
