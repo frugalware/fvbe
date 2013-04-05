c512760674e8add8a1b44b43425d80a578c340d0
James Buren <ryuo@frugalware.org>
2013-04-05 09:54:04 -0500

    add ishostname function for validating hostname strings

--- src/local.h
+++ src/local.h
@@ -127,6 +127,7 @@ struct tool
 };
 
 extern int charpp_qsort(const void *A,const void *B);
+extern bool ishostname(const char *name);
 extern void account_free(struct account *account);
 extern int get_number_padding(int n);
 extern bool find_unused_raid_device(struct raid **raids,char *s,size_t n);
