7099e8cbfacdcf7232241e3b02e812a3d208e1e9
James Buren <ryuo@frugalware.org>
2013-05-20 22:46:16 -0500

    langconfig: revise how systemd setup is used. only locale need be escaped because var is 100% a string literal we control, and thus need not be escaped.

--- src/langconfig.c
+++ src/langconfig.c
@@ -129,11 +129,7 @@ static bool update_via_new(void)
     return false;
   }
 
-  strfcpy(command,sizeof(command),"localectl set-locale");
-
-  strfcat(command,sizeof(command)," '%s=",shell_escape(var));
-
-  strfcat(command,sizeof(command),"%s'",shell_escape(locale));
+  strfcpy(command,sizeof(command),"localectl set-locale '%s=%s'",var,shell_escape(locale));
 
   for( ; vars[i] != 0 ; ++i )
   {
@@ -142,9 +138,7 @@ static bool update_via_new(void)
     if((locale = getenv(var)) == 0 || strlen(locale) == 0)
       continue;
 
-    strfcat(command,sizeof(command)," '%s=",shell_escape(var));
-
-    strfcat(command,sizeof(command),"%s'",shell_escape(locale));
+    strfcat(command,sizeof(command)," '%s=%s'",var,shell_escape(locale));
   }
 
   if(!execute(command,g->guestroot,0))
