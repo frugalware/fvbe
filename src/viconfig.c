5cf447a6f7ee4755c17bddbaadd6db50ebb06e16
James Buren <ryuo@frugalware.org>
2013-07-10 07:21:31 -0500

    viconfig: add function which checks the system to see what supported ncurses full screen text editors are present

--- src/viconfig.c
+++ src/viconfig.c
@@ -17,6 +17,68 @@
 
 #include "local.h"
 
+static char **editors = 0;
+
+static bool viconfig_setup_editors(void)
+{
+  static const char *list[] =
+  {
+    "vim",
+    "nano",
+    "vile",
+    "ne",
+    "dex",
+    "diakonos",
+    "jed",
+    "joe",
+    "jmacs",
+    "jpico",
+    "jstar",
+    0
+  };
+  const char *env = 0;
+  size_t i = 0;
+  const char *s = 0;
+  const char *e = 0;
+  char path[PATH_MAX] = {0};
+  struct stat st = {0};
+  size_t j = 0;
+
+  if((env = getenv("PATH")) == 0)
+  {
+    error("PATH is not defined");
+    return false;
+  }
+
+  env = strdupa(env);
+
+  editors = alloc(char *,sizeof(list) / sizeof(*list));
+
+  for( ; list[i] != 0 ; ++i )
+  {
+    for( s = e = env ; *e != 0 ; s = e )
+    {
+      if((e = strchr(e,':')) == 0)
+        e = s + strlen(s);
+      
+      strfcpy(path,sizeof(path),"%.*s/%s",(int) (e-s),s,list[i]);
+      
+      if(stat(path,&st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & 0755) == 0755)
+      {
+        editors[j++] = strdup(path);
+        break;
+      }
+      
+      if(*e == ':')
+        ++e;
+    }
+  }
+
+  editors[j] = 0;
+  
+  return true;
+}
+
 static bool viconfig_start(void)
 {
   return true;
