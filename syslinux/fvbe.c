3faa68f4fc658ffcb8140ba26e0ceae2a0002d77
James Buren <ryuo@frugalware.org>
2013-11-10 04:04:25 -0600

    syslinux: generate layouts menu

--- syslinux/fvbe.c
+++ syslinux/fvbe.c
@@ -5,7 +5,12 @@
 #include <unistd.h>
 #include <sys/pci.h>
 #include <errno.h>
+#include <limits.h>
 #include <cmenu.h>
+
+#ifndef LINE_MAX
+#define LINE_MAX 2048
+#endif
 
 #define RETURN_MAIN_MENU_TEXT "Return to Main Menu"
 #define error(S) fprintf(stdout,"%s: %s\n",__func__,S)
@@ -73,7 +78,7 @@ static bool pci_bus_probe(void)
 static bool locale_menu_setup(void)
 {
   FILE *file = 0;
-  char line[2048] = {0};
+  char line[LINE_MAX] = {0};
   char *p = 0;
 
   if((file = fopen("locales","rb")) == 0)
@@ -83,6 +88,35 @@ static bool locale_menu_setup(void)
   }
 
   add_named_menu("locale","Locale Selection",-1);
+
+  add_item(RETURN_MAIN_MENU_TEXT,"",OPT_EXITMENU,"main",0);
+
+  while(fgets(line,sizeof(line),file) != 0)
+  {
+    if((p = strchr(line,'\n')) != 0)
+      *p = 0;
+
+    add_item(line,"",OPT_EXITMENU,"main",0);
+  }
+
+  fclose(file);
+
+  return true;
+}
+
+static bool layout_menu_setup(void)
+{
+  FILE *file = 0;
+  char line[LINE_MAX] = {0};
+  char *p = 0;
+
+  if((file = fopen("layouts","rb")) == 0)
+  {
+    error("failed to open layouts file");
+    return false;
+  }
+
+  add_named_menu("layout","Keyboard Layout Selection",-1);
 
   add_item(RETURN_MAIN_MENU_TEXT,"",OPT_EXITMENU,"main",0);
 
