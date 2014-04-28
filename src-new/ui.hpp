f5bba9e3d432c5cbb1b0fb18dba1694741f432c3
James Buren <ryuo@frugalware.org>
2014-04-28 01:54:07 -0500

    begin rewrite of the UI generic interface in C++. eventually hope to use C++ to rewrite all the first revision of installer.

---
+++ src-new/ui.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class UI
+{
+
+	public:
+		static const unsigned char WIDGET_ALIGN_LEFT;
+		static const unsigned char WIDGET_ALIGN_RIGHT;
+		static const unsigned char WIDGET_ALIGN_UP;
+		static const unsigned char WIDGET_ALIGN_DOWN;
+
+};
