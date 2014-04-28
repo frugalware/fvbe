7cc63638b67f4f9337fb322c2fd4cf7b416e3d88
James Buren <ryuo@frugalware.org>
2014-04-28 04:56:06 -0500

    add a function which disables / enables signals interrupting system calls. needed to workaround an issue that was already fixed in installer V1. SIGWINCH would cause system calls to fail.

--- src-new/ui.cpp
+++ src-new/ui.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <csignal>
+#include <cstring>
 #include "ui.hpp"
 
 // UI Class Start
 void UI::printError(const std::string &line)
 {
 	std::cerr << line << std::endl;
+}
+
+bool UI::setSignalInterrupt(int sig, bool interrupt)
+{
+	struct sigaction sig_handler;
+
+	memset(&sig_handler, 0, sizeof(struct sigaction));
+
+	// Get the old signal handler.
+	if(sigaction(sig, NULL, &sig_handler) == -1)
+	{
+		printError("Failed to get old signal handler.");
+		return false;
+	}
+
+	if(interrupt && (sig_handler.sa_flags & SA_RESTART))
+	{
+		sig_handler.sa_flags ^= SA_RESTART;
+	}
+	else if(!interrupt && !(sig_handler.sa_flags & SA_RESTART))
+	{
+		sig_handler.sa_flags |= SA_RESTART;
+	}
+
+	// Set the new signal handler.
+	if(sigaction(sig, &sig_handler, NULL) == -1)
+	{
+		printError("Failed to set new signal handler.");
+		return false;
+	}
+
+	return true;
 }
 // UI Class End
 
