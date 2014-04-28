8868429ea139b2021a64b5a47dcd052aefcafad7
James Buren <ryuo@frugalware.org>
2014-04-28 02:20:00 -0500

    commit initial Widget class

--- src-new/ui.cpp
+++ src-new/ui.cpp
@@ -1,5 +1,6 @@
 #include "ui.hpp"
 
+// UI Class Start
 int UI::main(int argc, char **argv)
 {
 	(void) argc;
@@ -7,3 +8,14 @@ int UI::main(int argc, char **argv)
 
 	return 0;
 }
+// UI Class End
+
+// Widget Class Start
+const unsigned char Widget::ALIGN_LEFT        = (1 << 0);
+const unsigned char Widget::ALIGN_RIGHT       = (1 << 1);
+const unsigned char Widget::ALIGN_TOP         = (1 << 2);
+const unsigned char Widget::ALIGN_BOTTOM      = (1 << 3);
+
+const unsigned char Widget::EXPAND_HORIZONTAL = (1 << 0);
+const unsigned char Widget::EXPAND_VERTICAL   = (1 << 1);
+// Widget Class End
--- src-new/ui.hpp
+++ src-new/ui.hpp
@@ -7,3 +7,18 @@ class UI
 		virtual int main(int argc, char **argv);
 
 };
+
+class Widget
+{
+
+	public:
+
+	static const unsigned char ALIGN_LEFT;
+	static const unsigned char ALIGN_RIGHT;
+	static const unsigned char ALIGN_TOP;
+	static const unsigned char ALIGN_BOTTOM;
+
+	static const unsigned char EXPAND_HORIZONTAL;
+	static const unsigned char EXPAND_VERTICAL;
+
+};
