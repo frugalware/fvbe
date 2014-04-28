f1904d924168da8a67f97a956bab6c8f857dab8a
James Buren <ryuo@frugalware.org>
2014-04-28 02:06:03 -0500

    implement the new UI main as a virtual class function

--- src-new/ui.cpp
+++ src-new/ui.cpp
@@ -1,6 +1,9 @@
 #include "ui.hpp"
 
-const unsigned char UI::WIDGET_ALIGN_LEFT  = (1 << 0);
-const unsigned char UI::WIDGET_ALIGN_RIGHT = (1 << 1);
-const unsigned char UI::WIDGET_ALIGN_UP    = (1 << 2);
-const unsigned char UI::WIDGET_ALIGN_DOWN  = (1 << 3);
+int UI::main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	return 0;
+}
--- src-new/ui.hpp
+++ src-new/ui.hpp
@@ -4,9 +4,6 @@ class UI
 {
 
 	public:
-		static const unsigned char WIDGET_ALIGN_LEFT;
-		static const unsigned char WIDGET_ALIGN_RIGHT;
-		static const unsigned char WIDGET_ALIGN_UP;
-		static const unsigned char WIDGET_ALIGN_DOWN;
+		virtual int main(int argc, char **argv);
 
 };
